3e5734b7c23ffae13a2e0ac39a10f7427bc5b117
vyshak7 <33339458+vyshak7@users.noreply.github.com>
2017-11-03 15:35:33 +0530

    student
    .doc

---
+++ Student.java
@@ -0,0 +1,36 @@
+package admin;
+
+public class Student {
+private int id;
+private String name;
+private int roll_no;
+private int age;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getRoll_no() {
+	return roll_no;
+}
+public void setRoll_no(int roll_no) {
+	this.roll_no = roll_no;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+
+	
+	
+}
